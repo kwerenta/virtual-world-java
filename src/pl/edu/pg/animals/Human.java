503c492e1f5568f844b0fa5875a99e58db982a0e
Kamil Wenta <kamil.wenta222@gmail.com>
2023-05-15 11:56:24 +0200

    Add Human superability

--- src/pl/edu/pg/animals/Human.java
+++ src/pl/edu/pg/animals/Human.java
@@ -7,21 +7,34 @@ import pl.edu.pg.World;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.util.Random;
 
 public class Human extends Animal {
+    private static final int ABILITY_COOLDOWN = 5;
+    private static final int ABILITY_LENGTH = 5;
+    private static final int ABILITY_RANGE = 2;
     private Point queuedPosition;
+    private int abilityTimer = 1 - ABILITY_COOLDOWN;
 
     public Human(World world, Point position, int age) {
         super(world, position, age, 5, 4);
     }
 
     public void handleUserInput(int keyCode) {
+        Random rand = new Random();
         Point newPosition = new Point(position);
+        int range = 1;
+        if (abilityTimer >= 3 || (abilityTimer > 0 && rand.nextBoolean())) {
+            range = ABILITY_RANGE;
+            getWorld().addLog("Your ability worked");
+        }
+
         switch (keyCode) {
-            case KeyEvent.VK_UP -> newPosition.translate(0, -1);
-            case KeyEvent.VK_RIGHT -> newPosition.translate(1, 0);
-            case KeyEvent.VK_DOWN -> newPosition.translate(0, 1);
-            case KeyEvent.VK_LEFT -> newPosition.translate(-1, 0);
+            case KeyEvent.VK_UP -> newPosition.translate(0, -1 * range);
+            case KeyEvent.VK_RIGHT -> newPosition.translate(range, 0);
+            case KeyEvent.VK_DOWN -> newPosition.translate(0, range);
+            case KeyEvent.VK_LEFT -> newPosition.translate(-1 * range, 0);
+            case KeyEvent.VK_F -> ability();
             default -> {
                 return;
             }
@@ -32,7 +45,11 @@ public class Human extends Animal {
 
     @Override
     public void action() {
+        if (abilityTimer > 1 - ABILITY_COOLDOWN)
+            abilityTimer--;
+
         if (queuedPosition == null) return;
+
         Organism target = getWorld().getMap(queuedPosition);
         if (target == null)
             move(queuedPosition);
@@ -55,5 +72,16 @@ public class Human extends Animal {
     @Override
     public Species getSpecies() {
         return Species.HUMAN;
+    }
+
+    private void ability() {
+        if (abilityTimer > 0)
+            getWorld().addLog("Your ability is still active for " + abilityTimer + " turn(s)");
+        else if (abilityTimer > 1 - ABILITY_COOLDOWN)
+            getWorld().addLog("Your ability will be available in " + (ABILITY_COOLDOWN + abilityTimer - 1) + " turn(s)");
+        else {
+            getWorld().addLog("Your ability has been enabled");
+            abilityTimer = ABILITY_LENGTH;
+        }
     }
 }
